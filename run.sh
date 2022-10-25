25db5a679d87ead9b13dfb953b182fd5748c4aa9
1-Potato-1 <michal.vanhooft@gmail.com>
2022-10-25 08:27:28 +0200

    Remove unnecessary dependency on PyInstaller

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ python3 -m venv venv
 source ./venv/bin/activate
 python3 -m pip install --upgrade pip
 pip3 install poetry==1.0.0
-pip3 install PyInstaller==4.3
 poetry install
 # opencv has conflicting dependencies, so can't complete conflict resolution
 pip3 install opencv-python
